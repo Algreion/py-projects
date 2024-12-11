9092d4e16a76ab3ddfedd3b8a0c3af5157bb4e2f
Algreion <188793676+Algreion@users.noreply.github.com>
2024-12-11 13:05:37 +0100

    Day 11 solution

--- advent-calendar2024.py
+++ advent-calendar2024.py
@@ -466,3 +466,31 @@ def day10_solve(input: str, part2=False) -> int: # Ans1: 531 | Ans2: 1210
         for c in range(len(grid[0])):
             if grid[r][c] == 0: paths += d10_find_paths(grid, r, c, part2)
     return paths
+
+def d11_blink(stones: list, blinks) -> list:
+    def blink(s, depth=0):
+        key = (s, depth)
+        if depth >= blinks: return 1
+        if key in values:
+            return values[key]
+        if s == 0:
+            score = blink(1, depth+1)
+        elif not len(str(s)) % 2 and s>9:
+            s = str(s)
+            s1 = int(s[:len(s)//2])
+            s2 = int(s[len(s)//2:])
+            score = blink(s1, depth+1) + blink(s2, depth+1)
+        else: 
+            score = blink(s*2024, depth+1)
+        values[key] = score
+        return score
+    total_score = 0
+    values = {}
+    for s in stones:
+        total_score += blink(s)
+    return total_score
+
+def day11_solve(input: str, blinks: int = 25) -> int: # Ans1: 186424 | Ans2: 219838428124832
+    stones = list(map(int, input.strip("\n").split(" ")))
+    return d11_blink(stones, blinks)
+
