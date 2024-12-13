39f38c9b9db473e2f962e24389e4430084693c3d
Algreion <188793676+Algreion@users.noreply.github.com>
2024-12-13 12:44:42 +0100

    Day 13 sol

--- advent-calendar2024.py
+++ advent-calendar2024.py
@@ -581,3 +581,89 @@ def day12_solve(input: str, part2=False) -> int: # Ans1: 1518548 | Ans2: 909564
         res += a[k] * p[k]
     return res
 
+def d13_parse(input: str) -> dict:
+    res = defaultdict(list)
+    i = 0
+    for line in input.splitlines():
+        if not line: 
+            i += 1
+            continue
+        line = line.replace("+"," ").replace(","," ").replace("="," ")
+        for n in line.split():
+            if n.isdigit(): res[i].append(int(n))
+    return res
+
+def d13_z3_a(n: list, file: str = "day_13-1.txt") -> None:
+    """Writes the assertion for one claw machine"""
+    with open(file,"a") as f:
+        i,ax,ay,bx,by,x,y = n[-1],n[0],n[1],n[2],n[3],n[4],n[5]
+        f.write(
+f"""
+(push)
+(declare-const Tokens{i} Int)
+(declare-const A{i} Int)
+(declare-const B{i} Int)
+(declare-const x{i} Int)
+(declare-const y{i} Int)
+
+(assert (and (= x{i} {x}) (= y{i} {y})))
+(assert (= Tokens{i} (+ B{i} (* 3 A{i}))))
+(assert (= x{i} (+ (* B{i} {bx}) (* A{i} {ax}))))
+(assert (= y{i} (+ (* B{i} {by}) (* A{i} {ay}))))
+
+(minimize Tokens{i})
+(check-sat)
+(get-value (Tokens{i}))
+(pop)
+\n""")
+
+def d13_z3_b(n: list, file: str = "day_13-2.txt") -> None:
+    """Writes the assertion for one claw machine"""
+    with open(file,"a") as f:
+        i,ax,ay,bx,by,x,y = n[-1],n[0],n[1],n[2],n[3],n[4],n[5]
+        f.write(
+    f"""
+(push)
+(declare-const Tokens{i} Int)
+(declare-const A{i} Int)
+(declare-const B{i} Int)
+(declare-const x{i} Int)
+(declare-const y{i} Int)
+
+(assert (and (= x{i} {x+10000000000000}) (= y{i} {y+10000000000000})))
+(assert (= Tokens{i} (+ B{i} (* 3 A{i}))))
+(assert (= x{i} (+ (* B{i} {bx}) (* A{i} {ax}))))
+(assert (= y{i} (+ (* B{i} {by}) (* A{i} {ay}))))
+
+(minimize Tokens{i})
+(check-sat)
+(get-value (Tokens{i}))
+(pop)
+    \n""")
+
+def d13_write(input: str) -> bool:
+    """Writes the full Z3 file"""
+    nums = d13_parse(input)
+    for i in nums:
+        nums[i].append(i)
+        d13_z3_a(nums[i])
+        d13_z3_b(nums[i])
+    return True
+
+def d13_solve(result: str) -> int:
+    """Reads the result from Z3"""
+    tokens = 0
+    for line in result.splitlines():
+        if len(line) > 33: continue
+        for n in line.strip("))").split():
+            if n.isdigit(): tokens += int(n)
+    return tokens
+
+# Instructions for day 13: 
+# 1. Run d13_write(input) for the raw puzzle input.
+# 2. Run the day_13-1.txt and day_13-2.txt files with Z3
+# 3. Copy paste the entire result of one of the two parts in d13_solve(result)
+
+# Answer 1: 37680
+# Answer 2: 87550094242995
+
