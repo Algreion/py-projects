17bc4df254121be007bf2de1de594b518425553a
Algreion <188793676+Algreion@users.noreply.github.com>
2024-12-19 16:53:31 +0100

    Day 19 sol

--- advent-calendar2024.py
+++ advent-calendar2024.py
@@ -3,6 +3,7 @@ import time
 from collections import defaultdict, deque
 from itertools import combinations
 import heapq
+from functools import cache
 
 def d1_distance_solve(arr):
     arr1, arr2 = arr
@@ -855,3 +856,22 @@ def day18_solve2(input: str, loading: bool = True) -> int: # Ans: 36,17
         if not ns % 100 and loading: print(ns)
         if d18_pathfind(d18_parse(input, ns)) == -1: return input.splitlines()[ns-1] # -1 because 0-indexed
 
+@cache
+def d19_check(pattern: str, towels: list) -> int:
+    n = 0
+    for t in towels:
+        if pattern == t: n += 1
+        if pattern.startswith(t): n += d19_check(pattern.removeprefix(t), towels)
+    return n
+
+def day19_solve(input: str) -> None: # Ans1: 302 | Ans2: 771745460576799
+    total1 = 0
+    total2 = 0
+    towels, patterns = input.splitlines()[0], input.splitlines()[1:]
+    towels = tuple(sorted([t for t in towels.split(", ")], key=len, reverse=True))
+    for p in patterns:
+        x = d19_check(p, towels)
+        total2 += x
+        total1 += (x != 0)
+    print(f"Part 1 | Number of possible designs: {total1}")
+    print(f"Part 2 | Number of unique designs: {total2}")
