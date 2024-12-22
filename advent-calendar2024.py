ce89314c95914b26ec3df483f42764ae20985923
Algreion <188793676+Algreion@users.noreply.github.com>
2024-12-22 12:04:23 +0100

    Day 22 sol

--- advent-calendar2024.py
+++ advent-calendar2024.py
@@ -875,3 +875,21 @@ def day19_solve(input: str) -> None: # Ans1: 302 | Ans2: 771745460576799
         total1 += (x != 0)
     print(f"Part 1 | Number of possible designs: {total1}")
     print(f"Part 2 | Number of unique designs: {total2}")
+
+
+def d22_pr(n: int) -> int:
+    n ^= n<<6
+    n %= 1<<24
+    n ^= n>>5
+    n %= 1<<24
+    n ^= n<<11
+    n %= 1<<24
+    return n
+
+def d22_run(n: int) -> int:
+    for _ in range(2000):
+        n = d22_pr(n)
+    return n
+
+def day22_solve(input: str) -> int: # Ans: 13429191512
+    return sum(d22_run(int(n)) for n in input.splitlines())
