e3d351062d43f4c16638f7d8a272a7a9d3ca4e20
Algreion <188793676+Algreion@users.noreply.github.com>
2024-12-25 12:17:56 +0100

    Merry christmas!

--- advent-calendar2024.py
+++ advent-calendar2024.py
@@ -924,3 +924,27 @@ def day24_solve(result: str) -> int: # Ans: 49430469426918
         elif "false" in line: res += "0"
     return int(res, 2)
 
+def d25_parse(input: str) -> tuple:
+    keys, locks = {}, {}
+    input = [item.splitlines() for item in input.split("\n\n")]
+    h = len(input[0])-1
+    for item in input:
+        lock = "#" in item[0]
+        heights = {}
+        for i,line in enumerate(item):
+            for j, c in enumerate(line):
+                if lock and c != "#" and j not in heights: heights[j] = i-1
+                elif not lock and c == "#" and j not in heights: heights[j] = h-i
+        heights = tuple(v for _,v in sorted(heights.items()))
+        group = locks if lock else keys
+        group[heights] = group.get(heights, 0) + 1
+    return (keys, locks, h)
+
+def day25_solve(input: str) -> int: # Ans: 3663
+    keys, locks, h = d25_parse(input)
+    matches = 0
+    for k, k_i in keys.items():
+        for l, l_i in locks.items():
+            if all(k[i] + l[i] < h for i in range(len(k))): matches += k_i * l_i
+
+# Merry Christmas!
