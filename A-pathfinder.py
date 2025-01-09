6bacebddbe14c18db204bc36071f15c065d04031
Algreion <188793676+Algreion@users.noreply.github.com>
2025-01-09 18:23:55 +0100

    Major performance optimization, new features and custom mazes
    PERFORMANCE
    - Empty grid: x2.1 boost (1.18s -> 0.56s)
    - Hard maze: x2.9 boost (11.35s -> 3.91s)
    - Visualizer + Empty grid: x30 boost (150s -> 5s)
    - Visualizer + Hard maze: x100+ (too long -> 12.83s)
    FEATURES
    + Added ability to clear the grid
    + Added ability to save the current grid
    + Added a stopwatch to time pathfinding
    + Added a way to load in custom grids/mazes
    + Added a way to change distance calculation dynamically
    - Critical optimization in visualizer.
    
    MISC
    - Fixed a few bugs
    - Changed the color scheme slightly

--- A-pathfinder.py
+++ A-pathfinder.py
@@ -1,16 +1,32 @@
-import pygame
-import math
+import pygame, heapq
 from tkinter import messagebox
-import random
+from time import perf_counter as perf
+from math import sqrt
+from random import choice
 
 #? A* Pathfinding algorithm
 # Unlike its predecessor Dijkstra, which only looks at the shortest path with a priority queue, this takes into
 # consideration the distance from the end as well, with a heuristic estimate (Euclidean/Manhattan distance).
 # The total cost thus is f = g + h, then it works the same as Dijkstra's algorithm with a priority queue
 
+custom = False
+TIMING = True
+DISTANCE = 1 # 0 Euclidean, 1 Manhattan distance
+
+def get_custom_grid(filename: str = "maze.txt") -> list:
+    grid = []
+    try:
+        with open(filename, "r") as f:
+            for line in f:
+                grid.append(list(filter(lambda x: x in "SE#.", line)))
+        return grid
+    except: return []
+if custom: CUSTOM_GRID = get_custom_grid()
+else: CUSTOM_GRID = []
+
 def mainloop():
+    global CUSTOM_GRID, TIMING, DISTANCE
     SHOW = False
-    CUSTOM_GRID = None
     CUSTOM = bool(CUSTOM_GRID)
 
     ST, ED = (0,0), (-1,-1)
@@ -19,37 +35,12 @@ def mainloop():
     modes = ["normal", "king", "horse", "diagonals", "jumper", "flash", "wallhugger","drunk","mirror", "wormhole"]
     border = False
 
-    BG = (0,7,20)
-    CB = False # Cell Borders
-    WALLS = (220,227,213)
-    TOCHECK = (30,175,14)
-    PROCESSED = (200,0,0)
-    PATH = (0,160,245)
-    START = "green"
-    END = "magenta"
-    BORDER = "gray80"
-    EMPTY_S, OBS_S, PATH_S, START_S, END_S = ".","#","O","S","E"
-    WIDTH = 800 + (COLS - 800%COLS)
-    HEIGHT = 800 + (ROWS - 800%ROWS)
-    w = h = min(WIDTH // COLS, HEIGHT // ROWS)
-
-    if border:
-        COLS += 2
-        ROWS += 2
-        if ST == (0, 0): ST = (1,1)
-        if ED == (-1, -1): ED = (-2, -2)
-
-
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("A* Pathfinding")
-    screen.fill(BG)
-
     class Node:
         def __init__(self, x, y):
             self.i = x
             self.j = y
-            self.f = 0 # Cost function: f = g + h
-            self.g = 0 # Cost from start node to current
+            self.f = float("inf") # Cost function: f = g + h
+            self.g = float("inf") # Cost from start node to current
             self.h = 0 # Heuristic estimate from current to end node
             self.neighbors = []
             self.previous = None
@@ -58,7 +49,7 @@ def mainloop():
             self.value = 1 # Default cost value for g
 
         def show(self, color, border):
-            if self.processed == False:
+            if not self.processed:
                 pygame.draw.rect(screen, color, (self.i * w, self.j * h, w, h), border)
                 pygame.display.update()
 
@@ -89,7 +80,7 @@ def mainloop():
                         for ddi, ddj in [(1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,1),(1,-1),(-1,-1)]:
                             if (di,dj) not in dirs and (not (0<=i+di+ddi<COLS) or not (0<=j+dj+ddj<ROWS) or grid[i+di+ddi][j+dj+ddj].obs): dirs.append((di,dj))
                 case "drunk":
-                    dirs = [(random.choice([-1, 0, 1]), random.choice([-1, 0, 1])) for _ in range(8)]
+                    dirs = [(choice([-1, 0, 1]), choice([-1, 0, 1])) for _ in range(8)]
                 case "mirror":
                     dirs = [(-i+j,-j+i),(0,-1),(0,1),(1,0),(-1,0)]
                 case "wormhole":
@@ -102,24 +93,46 @@ def mainloop():
                 if (0<=i+di<COLS) and (0<=j+dj<ROWS) and not grid[i+di][j+dj].obs:
                     self.neighbors.append(grid[i+di][j+dj])
 
-
-    toCheck = [] # Nodes to be evaluated
-    processed = [] # Evaluated nodes
-
     if CUSTOM:
         COLS, ROWS = len(CUSTOM_GRID), len(CUSTOM_GRID[0])
         grid = [[0 for _ in range(COLS)] for _ in range(ROWS)]
         for r in range(ROWS):
             for c in range(COLS):
                 grid[r][c] = Node(r, c)
-
     else:
         grid = [[0 for _ in range(ROWS)] for _ in range(COLS)]
-
         # Create nodes
         for i in range(COLS):
             for j in range(ROWS):
                 grid[i][j] = Node(i, j)
+
+    BG = (0,7,20)
+    CB = False # Cell Borders
+    WALLS = (220,227,210)
+    TOCHECK = (0,153,0) # (30,175,14)
+    PROCESSED = (200,0,0)
+    PATH = (0,110,245)
+    START = (0,255,0)
+    END = (255,0,255)
+    BORDER = (160,160,160)
+    EMPTY_S, OBS_S, PATH_S, START_S, END_S = ".","#","O","S","E"
+    WIDTH = 800 + (COLS - 800%COLS)
+    HEIGHT = 800 + (ROWS - 800%ROWS)
+    w = h = min(WIDTH // COLS, HEIGHT // ROWS)
+
+    if border:
+        COLS += 2
+        ROWS += 2
+        if ST == (0, 0): ST = (1,1)
+        if ED == (-1, -1): ED = (-2, -2)
+
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("A* Pathfinding")
+    screen.fill(BG)
+
+    toCheck = [] # Nodes to be evaluated
+    processed = set() # Evaluated nodes
 
     # Show the empty grid
     if CB:
@@ -144,18 +157,20 @@ def mainloop():
         start, end = None, None
         for r in range(ROWS):
             for c in range(COLS):
-                if CUSTOM_GRID[c][r] == "S": start = grid[r][c]
-                if CUSTOM_GRID[c][r] == "E": end = grid[r][c]
+                if CUSTOM_GRID[c][r] == "S": 
+                    start = grid[r][c]
+                if CUSTOM_GRID[c][r] == "E": 
+                    end = grid[r][c]
         if not start:
             for r in range(ROWS):
                 for c in range(COLS):
-                    if CUSTOM_GRID[r][c] == ".": 
+                    if CUSTOM_GRID[c][r] == ".": 
                         start = grid[r][c]
                         break
         if not end:
-            for r in range(ROWS):
-                for c in range(COLS):
-                    if CUSTOM_GRID[r][c] == ".": 
+            for r in range(ROWS-1,-1,-1):
+                for c in range(COLS-1,-1,-1):
+                    if CUSTOM_GRID[c][r] == ".": 
                         end = grid[r][c]
                         break
     else:
@@ -167,26 +182,23 @@ def mainloop():
     end.show(END, 0)
 
     def draw(pos):
-        x, y = pos[0], pos[1]
-        i, j = x // w, y // h
-        square = grid[i][j]
+        x, y = pos
+        square = grid[x // w][y // h]
         if not square.obs and square != start and square != end:
             square.obs = True
             square.show(WALLS, 0)
 
     def erase(pos):
-        x, y = pos[0], pos[1]
-        i, j = x // w, y // h
-        square = grid[i][j]
+        x, y = pos
+        square = grid[x // w][y // h]
         if square.obs and square != start and square != end:
             square.obs = False
             square.show(BG, 0)
             if CB: square.show(WALLS, 1)
 
     def weight(pos, n):
-        x, y = pos[0], pos[1]
-        i, j = x // w, y // h
-        square = grid[i][j]
+        x, y = pos
+        square = grid[x // w][y // h]
         if not square.obs and square != start and square != end:
             if n == 1:
                 square.value = 1
@@ -206,8 +218,8 @@ def mainloop():
                 square.show((0, 255, 0), 1)
 
     def reposition(item, pos):
-        global start,end
-        x, y = pos[0], pos[1]
+        nonlocal start,end
+        x, y = pos
         i, j = x // w, y // h
         if item == "start":
             if grid[i][j] == end or grid[i][j].obs: return
@@ -221,22 +233,34 @@ def mainloop():
             if CB: end.show(WALLS, 1)
             end = grid[i][j]
             end.show(END,0)
+    
+    def clear_board():
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c].obs:
+                    grid[r][c].obs = False
+        screen.fill(BG)
+        start.show(START, 0)
+        end.show(END, 0)
+        print("Cleared grid.")
 
     if CUSTOM:
         for r in range(ROWS):
             for c in range(COLS):
                 if CUSTOM_GRID[c][r] == "#": draw((r*w+1, c*h+1))
 
-
-    # Drawing time:
+    # Drawing section:
     drawing = True
     neg = 1 # Negative weights
     modeselect = False
     saving = False
     number_keys = {pygame.K_0: 0, pygame.K_1: 1, pygame.K_2: 2, pygame.K_3: 3, pygame.K_4: 4,
                 pygame.K_5: 5, pygame.K_6: 6, pygame.K_7: 7, pygame.K_8: 8, pygame.K_9: 9}
-    print("""\nLeft Click: Draw walls | Right Click: Erase walls | Space: Begin pathfinding
-    Numbers: Increase cell weights | N: Toggle negative weights\nP: Toggle show progress | S/E: Reposition Start/End | M + Numbers: Change movement type\nX: Save board""")
+    print("""Keybinds:
+    Left Click: Draw walls | Right Click: Erase walls | Space: Begin pathfinding
+    Number keys: Increase cell weights | N: Toggle negative weights
+    P: Toggle show process | S/E: Reposition Start/End | M + Numbers: Change movement type
+    X: Save board | C: Clear grid | D: Change cost calculation (Manhattan / Euclidean)""")
     while drawing:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -275,17 +299,22 @@ def mainloop():
                 elif event.key == pygame.K_m:
                     modeselect = not modeselect
                     print("Selecting Mode",f"{"[ON]" if modeselect else "[OFF]"}")
+                elif event.key == pygame.K_c:
+                    clear_board()
+                elif event.key == pygame.K_d:
+                    DISTANCE = not DISTANCE
+                    print(f"Pathfinding with {"Manhattan" if DISTANCE else "Euclidean"} distance.")
+                elif event.key == pygame.K_t:
+                    TIMING = not TIMING
+                    print(f"Toggled stopwatch {"[ON]" if TIMING else "[OFF]"}")
                 elif event.key == pygame.K_x:
                     saving = True
                     print("Current board:\n")
                     res = [[0 for _ in range(COLS)] for _ in range(ROWS)]
                     for r in range(ROWS):
                         for c in range(COLS):
-                            try: res[r][c] = START_S if start == grid[c][r] else END_S if end==grid[c][r] else OBS_S if grid[c][r].obs else EMPTY_S
-                            except: print(r,c)
-                    print("[")
-                    for line in res: print(line,",",sep="")
-                    print("]")
+                            res[r][c] = START_S if start == grid[c][r] else END_S if end==grid[c][r] else OBS_S if grid[c][r].obs else EMPTY_S
+                    for line in res: print("".join(line))
 
                 elif event.key in number_keys:
                     if modeselect:
@@ -298,7 +327,8 @@ def mainloop():
                         except (AttributeError,IndexError):
                             continue
 
-    toCheck.append(start)
+    toCheck.append((0, (start.i,start.j)))
+    toshow = set([start])
 
     # Add neighbors considering new obstacles
     for i in range(COLS):
@@ -306,77 +336,63 @@ def mainloop():
             grid[i][j].addNeighbors(grid)
 
     # A* Heuristic function
-    def heurisitic(node, end):
-        return math.sqrt((node.i - end.i)**2 + (node.j - end.j)**2)
+    def heuristic(node, end):
+        return abs(node.i - end.i) + abs(node.j - end.j) # Manhattan
+    def euclidean(node, end):
+        return sqrt((node.i - end.i)**2 + (node.j - end.j)**2)
+    start.g = 0
+    start.f = heuristic(start, end) if DISTANCE else euclidean(start, end)
 
     # Pathfinding
     def main():
-        global repeat
+        global repeat, TIMING
         """A* Pathfinding algorithm. You can draw obstacles on the screen, or increase the weight of squares using numbers.
         Press N to make negative weights. Press S to view the pathfinding process. Press Space to begin pathfinding."""
-        start.show(START, 0)
-        end.show(END, 0)
         if toCheck: # Check while nodes haven't been checked yet
-            lowestIndex = 0
-            for i in range(len(toCheck)): # Find lowest f score
-                if toCheck[i].f < toCheck[lowestIndex].f:
-                    lowestIndex = i
-
-            current = toCheck[lowestIndex]
+            _, (x,y) = heapq.heappop(toCheck)
+            current = grid[x][y]
             if current == end:
                 distance = int(current.f)
                 print("Path found. Distance:", distance)
-                start.show(START,0)
-                while current.previous: # Trace back previous path
+                if TIMING: print(f"{perf()-starttime:.3f}s")
+                while current != start: # Trace back previous path
                     current.processed = False
                     current.show(PATH, 0)
                     if saving and current != end: res[current.j][current.i] = PATH_S
                     current = current.previous
                 end.show(END, 0)
-
-                if saving: 
+                if saving:
                     print("Path:\n")
-                    for line in res: print(line,",",sep="")
-
-                if messagebox.askokcancel("Path found", ("A path has been found.\nThe lowest distance to the path is " + str(distance//1) + " blocks away.\nPress OK to try again, or cancel to exit.")):
+                    for line in res: print("".join(line))
+                if messagebox.askokcancel("Path found", (f"A path has been found.\nThe lowest distance to the path is {str(distance//1)} blocks away.\nPress OK to try again, or cancel to exit.")):
                     repeat = True
                 pygame.quit()
                 return
 
-            toCheck.pop(lowestIndex)
-            processed.append(current)
+            processed.add((x,y))
+            if SHOW and grid[x][y] != start: grid[x][y].show(PROCESSED, 0)
 
             neighbors = current.neighbors
-            for i in range(len(neighbors)): # Add neighbors
-                neighbor = neighbors[i]
-                if neighbor not in processed:
+            for neighbor in neighbors: # Add neighbors
+                a,b = neighbor.i, neighbor.j
+                if (a,b) not in processed:
                     tempG = current.g + current.value
-                    if neighbor in toCheck:
-                        if neighbor.g > tempG: # If this cost is smaller than before, you set it as the new cost (we found a shortest path to the node)
-                            neighbor.g = tempG
-                            neighbor.previous = current 
-                    else:
+                    if tempG < neighbor.g:
                         neighbor.g = tempG
-                        toCheck.append(neighbor)
-                        neighbor.previous = current 
+                        neighbor.h = heuristic(neighbor, end) if DISTANCE else euclidean(neighbor, end)
+                        neighbor.f = neighbor.g + neighbor.h
+                        neighbor.previous = current
+                        heapq.heappush(toCheck,(neighbor.f,(a,b)))
+                        if SHOW: neighbor.show(TOCHECK, 0)
+                        toshow.add(neighbor)
 
-                neighbor.h = heurisitic(neighbor, end)
-                neighbor.f = neighbor.g + neighbor.h
-                
-        if SHOW:
-            for i in range(len(toCheck)):
-                toCheck[i].show(TOCHECK, 0)
-
-            for i in range(len(processed)):
-                if processed[i] != start:
-                    processed[i].show(PROCESSED, 0)
-        
         if not toCheck:
             print("There is no path to the end node.")
             pygame.quit()
             return
         current.processed = True
-
+    
+    if TIMING: starttime = perf()
     while True:
         if not pygame.get_init():
             break
