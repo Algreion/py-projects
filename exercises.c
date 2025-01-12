3112b14d63154973f878e0741c33e8d3de52f628
Algreion <188793676+Algreion@users.noreply.github.com>
2025-01-12 22:10:18 +0100

    Bubblesort

--- exercises.c
+++ exercises.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
 void selection_sort(int t[], int n);
+void bubble_sort(int t[], int n);
 
 int main() {
-    int t[] = {5,4,3,2,1};
+    int t[] = {5,4,3,2,1,0};
+    int t2[] = {30,50,40,20,10};
     int n = sizeof(t) / sizeof(t[0]);
+    int n2 = sizeof(t2) / sizeof(t2[0]);
     selection_sort(t, n);
-    printf("Sorted array: \n");
+    bubble_sort(t2, n2);
+    printf("Selection sort: \n");
     for (int i = 0; i < n; i++) {
         printf("%d ", t[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < n2; i++) {
+        printf("%d ", t2[i]);
     }
     printf("\n");
     return 0;
@@ -25,5 +33,22 @@ void selection_sort(int t[], int n) {
         int temp = t[i];
         t[i] = t[index];
         t[index] = temp;
+    }
+}
+
+void bubble_sort (int t[], int n) {
+    int iters = 1;
+    int swapped = 1;
+    while (swapped == 1) {
+        swapped = 0;
+        for (int i=0; i < n-iters; i++) {
+            if (t[i] > t[i+1]) {
+                swapped = 1;
+                int temp = t[i];
+                t[i] = t[i + 1];
+                t[i + 1] = temp;
+            }
+        }
+        iters ++;
     }
 }
