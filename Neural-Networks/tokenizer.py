db7179fe1790f97ccb848c6c918eb1f878785cd1
Algreion <188793676+Algreion@users.noreply.github.com>
2025-02-09 19:18:43 +0100

    Fixed bug

--- Neural-Networks/tokenizer.py
+++ Neural-Networks/tokenizer.py
@@ -1,9 +1,11 @@
+
 class Tokenizer:
-    def __init__(self, charsFile: str = '', pairN: int = 2, charsDict: dict = {}):
+    def __init__(self, maxChars: int = 1000, charsFile: str = '', charsDict: dict = {}):
         self.chars = charsDict
         self.num = max(self.chars)+1 if self.chars else 256
-        self.pairN = pairN
+        self.maxChars = maxChars
         self.minNum = self.num
+        self.k = dict()
         if charsFile:
             self.loadchars(charsFile)
     
@@ -26,22 +28,42 @@ class Tokenizer:
     def merge(self, tokens: list) -> list:
         counts = sorted(((v,k) for k,v in self.findpairs(tokens).items()))
         n = self.num
-        while counts and counts[-1][0] >= self.pairN:
+        while counts and n <= self.maxChars and counts[-1][0] >= 2:
             _, (c1,c2) = counts.pop()
-            self.chars[n] = (c1,c2)
+            if (c1,c2) not in self.k:
+                self.chars[n] = (c1,c2)
+                num = n
+                n += 1
+            else: num = self.k[(c1,c2)]
             new = []
             i = 0
             while i < len(tokens):
                 if i < len(tokens)-1 and tokens[i] == c1 and tokens[i+1] == c2:
-                    new.append(n)
+                    new.append(num)
                     i += 2
                 else:
                     new.append(tokens[i])
                     i += 1
             tokens = new
-            n += 1
             counts = sorted(((v,k) for k,v in self.findpairs(tokens).items()))
+        self.k = dict([(v,k) for k,v in self.chars.items()])
+        if n >= self.maxChars:
+            check = True
+            while check and counts and counts[-1][0] >= 2:
+                check = False
+                new = []
+                i = 0
+                while i < len(tokens):
+                    if i < len(tokens)-1 and (tokens[i],tokens[i+1]) in self.k:
+                        new.append(self.k[(tokens[i],tokens[i+1])])
+                        i += 2
+                        check = True
+                    else:
+                        new.append(tokens[i])
+                        i += 1
+                tokens = new
         self.num = n
+        
         return tokens
     
     def unmerge(self, tokens: list) -> list:
