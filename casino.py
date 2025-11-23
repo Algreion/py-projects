e4e5b0c73d8a5a0431e6af715689fb3bdb70eac0
Algreion <188793676+Algreion@users.noreply.github.com>
2025-11-23 11:17:32 +0100

    Improved experience

--- casino.py
+++ casino.py
@@ -2,6 +2,8 @@ import random
 from collections import deque
 from rich import print,text
 
+#TODO | UNO, add cheating options with skill, finish casino mainloop, add chess, add roulette
+
 N2W = {1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine",10:"ten"}
 W2N = dict([(w,n) for n,w in N2W.items()])
 FACEDOWN = "purple"
@@ -11,9 +13,19 @@ BLACKJACK_HELP = """
 1 / H - Hit
 2 / S - Stand
 3 / D - Double down (x2 bet, take 1 card, stand)"""
+CASINO_HELP = """
+1 / P - Play a game
+2 / S - View current status
+3 / C - Manage characters
+4 / T - Train skill or earn money
+5 / Z - Cheats
+6 / X - Quit"""
+SKILLCAP = 100
+DEALERSKILL = 50
+SKILLRATE = 0.1 # Skill gain multiplier
 
 class Player:
-    def __init__(self, name: str = "Player", money: float = 100, skill: int = 0, age: int = 18):
+    def __init__(self, name: str= "Player", money: float = 100, skill: int = 0, age: int = 18):
         self.name = name
         self.age = age
         self.money = money
@@ -168,7 +180,7 @@ class Hand:
         """Adds a card to the hand."""
         self.hand.append(card)
         self.size = len(self)
-
+Player()
 class Casino:
     def __init__(self, name: str = "Casino", money: float = 1e6, dealer: Player | None = None):
         self.name = name
@@ -177,12 +189,107 @@ class Casino:
         self.ID = 0
         self.db = dict()
         self.dealer = dealer
-        self.games = {"bj": Blackjack(casino=self), "uno": UNO()}
+        self.games = {1: Blackjack(casino=self), 2: UNO()}
     def __repr__(self):
         return f"Casino(name={self.name},money={self.money},players={self.size})"
     def __str__(self):
         return self.name
-    def play(self, game: str = "bj", players: Player | list | None = None):
+    def create_char(self) -> Player:
+        x = input("Name Age\n> ").split()
+        match len(x):
+            case 0:
+                name, age = "Player", 18
+            case 1:
+                name, age = x[0], 18
+            case 2:
+                name, age = x[0],int(x[1])
+        return Player(name=name,age=age)
+    def choose_char(self, active) -> Player | None:
+        char = None
+        if len(active) > 1:
+            print("Choose a character. ")
+            x = input(" | ".join(f"{self.playerID(p)} - {p.name}" for p in active),'\n> ')
+            if x.isdigit() and int(x) in self.db:
+                char = self.db[int(x)]
+            else:
+                print("Invalid character. Cancelled operation.")
+        else: char = self.db[list(active)[0]]
+        return char
+    def mainloop(self):
+        """Full game experience."""
+        active = set()
+        main = -1
+        cheat = False
+        print(f"Welcome to [bold {FACEDOWN}]{self.name}[/bold {FACEDOWN}]!")
+        print("Your options are as follows. Type 'help' to view them again:",CASINO_HELP)
+        while True:
+            print(SEP)
+            if not active:
+                print("You have not yet created a character. Choose the following all in a single line, or leave blank to pick default: ")
+                char = self.create_char()
+                self.register(char)
+                i = self.playerID(char)
+                active,main = {i}, i
+                print(f"{self.db[i]} registered.")
+                continue
+            x = input(f"[{self.db[main].name}] > ").lower()
+            if x in ['1','p','play']:
+                print(f"Choose a game to play. Options:\n{' | '.join(f"{k} - {str(v)}" for k,v in self.games.items())}")
+                y = input("> ")
+                if not y.isdigit():
+                    print("Invalid option.")
+                else:
+                    self.play(int(y),[self.db[i] for i in active])
+            elif x in ['2','s','status']:
+                char = self.choose_char(active)
+                print(f"Name: {char.name} | Age: {char.age} | Money: {self.money_color(char.money)} | Skill: {char.skill}")
+                print(f"Wins: {char.wins} | Losses: {char.losses} | Win rate: {100*char.wins/max(1,(char.wins+char.losses))}%")
+            elif x in ['3','c','chars','characters']:
+                pass
+            elif x in ['4','t','train']:
+                pass
+            elif x in ['5','z','cheats','cheat']:
+                y = input("Cheat options:\n1. Change money | 2. Change skill | 3. Cancel debt\n> ")
+                match y:
+                    case '1':
+                        char = self.choose_char(active)
+                        if char is None: continue
+                        print(f"Character {char.name} has {self.money_color(char.money)}.")
+                        y = input("Input a new cash amount.\n>")
+                        try: y = float(y)
+                        except:
+                            print("Cancelled operation.")
+                            continue
+                        cheat = True
+                        char.money = y
+                        print(f"Set {char.name}'s money to {self.money_color(y)}.")
+                    case '2':
+                        char = self.choose_char(active)
+                        if char is None: continue
+                        print(f"Character {char.name} has {char.skill} skill.")
+                        y = input(f"Input a new skill value. {SKILLCAP} is the maximum.\n> ")
+                        try: y = float(y)
+                        except:
+                            print("Cancelled operation.")
+                            continue
+                        cheat = True
+                        char.skill = min(y,SKILLCAP)
+                        print(f"Set {char.name}'s skill to {char.skill}.")
+                    case '3':
+                        print("Your debt has been forgiven.")
+                        for i in active: self.db[i].money = max(self.db[i].money,0)
+                        cheat = True
+                    case _:
+                        print("Invalid option.")
+            elif x in ['6','q','quit','x']:
+                print(f"Thank you for playing, {self.db[main]}! You made a total of {self.money_color(sum([p.money for p in [self.db[i] for i in active]])-100*len(active))}.")
+                quit()
+            elif x in ['h','help','format']:
+                print(CASINO_HELP)
+            else:
+                print("Invalid option. Type 'help' to see all options.")
+    def play(self, game: int = 1, players: Player | list | None = None):
+        """Start up a specific game."""
         if game not in self.games:
             print("Game not available.")
         else:
@@ -197,6 +304,11 @@ class Casino:
                 for i in range(len(players)):
                     color = "red1" if gains[i]<0 else "green1" if gains[i]>0 else "white"
                     print(f"{players[i]} {"earned" if color=="green1" else "lost" if color=="red1" else "made"} [{color}]{abs(gains[i])}[/{color}]$.")
+    def money_color(self, amount: float) -> str:
+        """Returns the color coded amount."""
+        POS, NEG, NEUTRAL = "red1","green1","white"
+        color = "red1" if amount<0 else "green1" if amount>0 else "white"
+        return f"[{color}]{amount}$[/{color}]"
     def players(self):
         return list(self.db.values())
     def playerID(self, player: Player) -> int | None:
@@ -221,13 +333,16 @@ class Blackjack:
         """States: BETTING | DEAL | PLAYER_TURN | DEALER_TURN | PAYOUT"""
         self.deck = Deck("classic")
         self.casino = casino
-        self.dealer = casino.dealer if casino is not None else Player("Dealer")
+        self.dealer = casino.dealer if casino is not None else Player("Dealer",skill=DEALERSKILL)
         self.dealerhand = Hand(self.dealer,"Blackjack")
         self.rounds = 1
+        self.skillgains = [1,0.5,0.25] # Win, Tie, Loss skill increase.
         self.init_players(players)
         self.deck.shuffle()
     def __repr__(self):
         return f"Blackjack(players={len(self.players)},rounds={self.rounds},dealer={self.dealer.name})"
+    def __str__(self):
+        return "Blackjack"
     def init_players(self, players: Player | list | None = None):
         self.players = None
         if players is None:
@@ -283,10 +398,12 @@ class Blackjack:
         for p in self.players:
             x = self.determine(self.playerhands[p], info=p.name)
             p.money += x*self.bets[p]
+            p.skill += SKILLRATE*self.skillgains[0 if x>0 else 2 if x<0 else 1]
             if info and x!=0: print(f"{p.name} {"earns" if int(x>0) else "pays"} {abs(x*self.bets[p])}$!")
             if self.casino is not None: self.casino.money -= x*self.bets[p]
             p.wins += int(x>0)
             p.losses += int(x<0)
+            p.money,p.skill = round(p.money,2),round(p.skill,2)
     def determine(self, hand: Hand, info: str | None = None) -> float:
         """Returns the payout and win/loss info for specific player."""
         value = self.value(hand)
@@ -307,7 +424,7 @@ class Blackjack:
             if info: print(f"[red1]Dealer wins against {info}.[/red1]")
             return -1
         else:
-            if info:  print(f"{info} tied.")
+            if info:  print(f"[cyan]{info} tied.[/cyan]")
             return 0
     def player_turn(self, player: Player):
         """Handles an individual player's turn."""
@@ -319,7 +436,10 @@ class Blackjack:
             v = self.value(self.playerhands[player])
             print(f"Value: {v}")
             if v == 21:
-                print("[green1]Natural blackjack![/green1]")
+                if len(self.playerhands[player]) == 2:
+                    print("[green1]Natural blackjack![/green1]")
+                else:
+                    print("[green1]Blackjack![/green1]")
                 break
             x = input(f"\n[{player.name}] > ").lower()
             if x in ['1','h','hit']:
@@ -364,20 +484,20 @@ class Blackjack:
                 self.bets[p] = 100.0
         # Dealing
         print(SEP)
-        print("\nDealing cards...\n")
+        print("\n[pink1]Dealing cards...[pink1]\n")
         self.deal()
-        print("Dealer's hand:",self.dealerhand,f"({self.value(self.dealerhand)})")
+        print("Dealer's hand:",self.dealerhand)
         print(SEP)
         # Player turns
         for p in self.players:
             self.player_turn(p)
         # Dealer's turn
-        print("\nThe dealer is playing...")
+        print("[pink1]The dealer is playing...[/pink1]\n")
         self.dealer_turn()
         print("Dealer's hand:",self.dealerhand)
         # Payout
-        print(f"Dealer's value: {self.value(self.dealerhand)}")
-        print("Results:")
+        print(f"Value: {self.value(self.dealerhand)}")
+        print("\nResults:")
         self.payout(info=True)
         self.rounds += 1
     def mainloop(self, players: Player | list | None = None) -> int:
@@ -390,8 +510,8 @@ class Blackjack:
             print(SEP)
             print(f"""Player options are as follows. Type 'help' to see them again:{BLACKJACK_HELP}""")
             print(SEP)
-        print(f"\nHello {', '.join([p.name for p in players])}! You have played {self.rounds-1} rounds. Want to play {"again" if self.rounds>1 else '(Enter/X)'}?")
-        while input(f"Play {"again" if self.rounds>1 else '(Enter/X)'}?\n> ").lower() not in ['x','0',"no",'n']: 
+        print(f"\nHello {', '.join([p.name for p in players])}! You have played {self.rounds-1} rounds. Want to play{" again" if self.rounds>1 else ''}?")
+        while input(f"Play {"again" if self.rounds>1 else '(Enter/X)'}?\n> ").lower() in ['','y','yes','1','sure','ok','play']: 
             self.round()
             print(SEP)
             self.reset()
@@ -407,3 +527,9 @@ class UNO:
         self.casino = casino
     def __repr__(self):
         return f"UNO({f"casino={self.casino}" if self.casino is not None else ''})"
+    def __str__(self):
+        return "UNO"
+
+if __name__ == '__main__':
+    casino = Casino("Crystal Palace")
+    casino.mainloop()
